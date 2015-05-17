765a25579187b18ead5282720ca1c4047c4aeca6
frankie <max00355@gmail.com>
2015-05-17 18:48:09 -0400

    Can now accept trade offers

--- PyTradeBot.py
+++ PyTradeBot.py
@@ -1,13 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import re
 
 class Bot:
-    def __init__(self, username, password):
+    def __init__(self, username, password, steamid, debug=False):
         self.username = username
         self.password = password
+        self.steamid = steamid
         self.loginURL = "http://store.steampowered.com/login/"
-        self.driver = webdriver.Firefox()
+        if debug:
+            self.driver = webdriver.Firefox()
+        else:
+            self.driver = webdriver.PhantomJS()
+            self.driver.set_window_size(0, 0)
 
     def login(self):
         print "Logging in, make sure SteamGuard is disabled"
@@ -18,8 +24,15 @@ class Bot:
         password.send_keys(self.password)
         password.send_keys(Keys.RETURN)
         print "Logged in! I didn't check and make sure the username and password was correct, so make sure they are."
-        raw_input("Press enter")
     
-    def trade(self):
-        self.driver.get("http://steamcommunity.com/profiles/76561198104150539")
+    def sendTrade(self, url):
+        self.driver.get(url)
+        print "Trade request sent"
 
+    
+    def incomingTrade(self):
+        self.driver.get("http://steamcommunity.com/profiles/{}/tradeoffers/".format(self.steamid))
+        tradeUrl = re.findall("javascript:ShowTradeOffer(.*);", self.driver.page_source)[0].replace("(", '').replace("'", '').replace(")",'').replace(" ", '')
+        self.driver.get("https://steamcommunity.com/tradeoffer/{}/".format(tradeUrl))
+        self.driver.find_element_by_id("you_notready").click() 
+        self.driver.find_element_by_id("trade_confirmbtn").click()
